0146d047b20c4e450008df4ac2000e1d72e8cdc1
ComedyStudios <mishaprudnikov11@gmail.com>
2022-01-15 18:03:08 +0100

    a black background can be generated

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Engine_3D
 {
@@ -20,9 +10,55 @@ namespace Engine_3D
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static int WindowHeight;
+        private static int WindowWidth;
+        private WriteableBitmap WriteableBitmap;
         public MainWindow()
         {
             InitializeComponent();
+            DrawBackground();
+        }
+
+        private void DrawBackground()
+        {
+            WindowWidth = (int) MainGrid.Width;
+            WindowHeight = (int) MainGrid.Height;
+            WriteableBitmap = new WriteableBitmap(WindowWidth, WindowHeight, 96, 96, PixelFormats.Bgr32, null);
+            byte[, ,] pixels = new byte[WindowHeight, WindowWidth, 4];
+            for (int x = 0;x< WriteableBitmap.Width ;x++)
+            {
+                for (int y = 0;y< WriteableBitmap.Width;y++)
+                {
+                    for (int i = 0; i < 3; i++)
+                        pixels[y, x, i] = 0;
+                    pixels[y, x, 3] = 255;
+                }
+            }
+            
+            byte[] pixels1d = new byte[WindowHeight * WindowWidth * 4];
+            int index = 0;
+            for (int row = 0; row < WindowHeight; row++)
+            {
+                for (int col = 0; col < WindowWidth; col++)
+                {
+                    for (int i = 0; i < 4; i++)
+                        pixels1d[index++]= pixels[row, col, i];
+                }
+            }
+            // Update writeable bitmap with the colorArray to the image.
+            Int32Rect rect = new Int32Rect(0, 0, WindowWidth, WindowHeight);
+            int stride = 4 * WindowWidth;
+            WriteableBitmap.WritePixels(rect, pixels1d, stride, 0);
+
+            // Create an Image to display the bitmap.
+            Image image = new Image();
+            image.Stretch = Stretch.None;
+            image.Margin = new Thickness(0);
+
+            MainGrid.Children.Add(image);
+
+            //Set the Image source.
+            image.Source = WriteableBitmap;
         }
     }
 }
